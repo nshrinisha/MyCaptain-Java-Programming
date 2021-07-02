b4e9ac61a98863d99f29aa833dfba9ba20373dc7
Shrinisha N <70162152+nshrinisha@users.noreply.github.com>
2021-07-02 11:35:34 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,13 @@
+public class Helloworld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
+/**
+ * 
+PS C:\Users\HP\Desktop\Mycaptain_Java> javac Helloworld.java
+PS C:\Users\HP\Desktop\Mycaptain_Java> java Helloworld
+Hello World
+ */
