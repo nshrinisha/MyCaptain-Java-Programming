60f1d904e6519426cbb50b986b58346166248642
Shrinisha N <70162152+nshrinisha@users.noreply.github.com>
2021-07-25 15:48:55 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,50 @@
+//FIBONACCI USING RECURSION
+class FiboWithRec{  
+    static int n1=0,n2=1,n3=0;    
+    static void printFibonacci(int count){    
+       if(count>0){    
+            n3 = n1 + n2;    
+            n1 = n2;    
+            n2 = n3;    
+            System.out.print(" "+n3);   
+            printFibonacci(count-1);    
+        }    
+    }    
+    public static void main(String args[]){    
+     int count=10;    
+     System.out.print(n1+" "+n2);//printing 0 and 1    
+     printFibonacci(count-2);//n-2 because 2 numbers are already printed   
+    }  
+   }  
+
+/*OUTPUT:
+C:\Users\HP\Desktop\Mycaptain\Mycaptain_Java>javac FiboWithRec.java
+
+C:\Users\HP\Desktop\Mycaptain\Mycaptain_Java>java FiboWithRec
+0 1 1 2 3 5 8 13 21 34
+*/
+
+//FIBONACCI WITHOUT USING RECURSION
+class FiboWithoutRec{  
+    public static void main(String args[])  
+    {    
+     int n1=0,n2=1,n3,i,count=10;    
+     System.out.print(n1+" "+n2);//printing 0 and 1    
+        
+     for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
+     {    
+      n3=n1+n2;    
+      System.out.print(" "+n3);    
+      n1=n2;    
+      n2=n3;    
+     }    
+      
+    }
+}  
+/*
+OUTPUT
+C:\Users\HP\Desktop\Mycaptain\Mycaptain_Java>javac FiboWithoutRec.java
+
+C:\Users\HP\Desktop\Mycaptain\Mycaptain_Java>java FiboWithoutRec
+0 1 1 2 3 5 8 13 21 34
+*/
